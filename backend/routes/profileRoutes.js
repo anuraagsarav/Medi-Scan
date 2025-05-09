5d604f5175ef6baadde1badefe240da5958f843c
Anuraag S Sarav <126107012+anuraagsarav@users.noreply.github.com>
2025-05-09 08:46:50 +0530

    Delete backend directory

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { updateFullProfile } = require("../controllers/profileController");
-const protect = require("../middlewares/authMiddleware");
-
-const router = express.Router();
-
-router.put("/full-update", protect, updateFullProfile);
-
-module.exports = router;
--- backend/utils/hashPassword.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require("bcrypt");
-
-exports.hash = async (password) => await bcrypt.hash(password, 10);
-exports.compare = async (input, hashed) => await bcrypt.compare(input, hashed);
