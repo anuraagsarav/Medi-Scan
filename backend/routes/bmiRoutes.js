37b8b14675dd4dd3fd71c679369d1aa2a4fe8c90
Anuraag S Sarav <126107012+anuraagsarav@users.noreply.github.com>
2025-05-09 08:51:49 +0530

    deleted

--- backend/routes/bmiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateDietPlan } = require('../controllers/bmiController');
-
-router.post('/diet', generateDietPlan);
-
-module.exports = router;
